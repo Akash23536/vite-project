8551b564070427834d082202edd2d0c7b2e20ab4
Akash Bhadana <23536akash.2021@gmail.com>
2024-06-11 22:30:22 +0530

    COUNTER BY INCREMENT BY +1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  const [fact, setFact] = useState({
-    color: "Blue",
-    number: 74,
-    fruit: "Mango",
-  });
+  const [COUNT, SETCOUNT] = useState(0);
 
   const changeColor = () => {
-    setFact((e) => {
-      return { ...e, number: 100 };
-    });
+    SETCOUNT(COUNT + 1);
   };
   return (
     <>
-      <h1>
-        My fav Color :{fact.color} and my Fav No : {fact.number} and Fruit :
-        {fact.fruit}
-      </h1>
-      <button onClick={changeColor}>change</button>
+      <h1>COUNT: {COUNT}</h1>
+
+      <button onClick={changeColor}>COUNTER</button>
     </>
   );
 }
