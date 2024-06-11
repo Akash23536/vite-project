b774e8f1e9b37803c42aa8b4381f8fb549fc983c
Akash Bhadana <23536akash.2021@gmail.com>
2024-06-11 22:37:42 +0530

    interview Question to increase the counter +4

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ function App() {
   const [COUNT, SETCOUNT] = useState(0);
 
   const changeColor = () => {
-    SETCOUNT(COUNT + 1);
+    // SETCOUNT(COUNT + 1); //1
+    // SETCOUNT(COUNT + 1); //1
+    // SETCOUNT(COUNT + 1); //1
+    // SETCOUNT(COUNT + 1); //1
+
+    SETCOUNT((e) => e + 1); //1
+    SETCOUNT((e) => e + 1); //2
+    SETCOUNT((e) => e + 1); //3
+    SETCOUNT((e) => e + 1); //4
   };
   return (
     <>
       <h1>COUNT: {COUNT}</h1>
-
       <button onClick={changeColor}>COUNTER</button>
     </>
   );
