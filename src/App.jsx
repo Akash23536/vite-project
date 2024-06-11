d12ddfa92a01afd1da7a0f1b18a71a3c254b2363
Akash Bhadana <23536akash.2021@gmail.com>
2024-06-11 22:17:08 +0530

    Need of spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("Red");
+  const [fact, setFact] = useState({
+    color: "Blue",
+    number: 74,
+    fruit: "Mango",
+  });
 
   const changeColor = () => {
-    setColor("Black");
+    setFact({
+      number: "0001",
+    });
   };
   return (
     <>
-      <h1>My fav Color is {color}</h1>
+      <h1>
+        My fav Color :{fact.color} and my Fav No : {fact.number} and Fruit :{fact.fruit}
+      </h1>
       <button onClick={changeColor}>change</button>
     </>
   );
