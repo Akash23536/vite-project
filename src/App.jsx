151e23008d17d550c40f3c511ee5824f824f2d34
Akash Bhadana <23536akash.2021@gmail.com>
2024-06-11 22:05:28 +0530

    Need of useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import ProfileCard from "./Components/ProfileCard"
-
-
 function App() {
+  let color = "Blue";
 
+  const changeColor = () => {
+    color = "Black";
+    console.log(color);
+  };
   return (
-    <> 
-       <ProfileCard />
+    <>
+      <h1>My fav Color is {color}</h1>
+      <button onClick={changeColor}>change</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
